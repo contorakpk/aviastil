d8d2e6d3850b07f1b06a28e58bb6922a9a7923f6
FluSett <flusett@gmail.com>
2021-01-27 13:32:44 +0200

    0.2

--- lib/pages/admin_page.dart
+++ lib/pages/admin_page.dart
@@ -1,29 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
+import '../widgets/input/input_field.dart';
+
 class AdminPage extends StatefulWidget {
   @override
   _AdminPageState createState() => _AdminPageState();
 }
 
 class _AdminPageState extends State<AdminPage> {
-  _openPopup(context) {
+  TextEditingController _firstController = TextEditingController();
+  TextEditingController _secondController = TextEditingController();
+  TextEditingController _thirdController = TextEditingController();
+  TextEditingController _fourthController = TextEditingController();
+  TextEditingController _fifthController = TextEditingController();
+  TextEditingController _sixthController = TextEditingController();
+  TextEditingController _seventhController = TextEditingController();
+
+  void _clearControllers() {
+    _firstController.clear();
+    _secondController.clear();
+    _thirdController.clear();
+    _fourthController.clear();
+    _fifthController.clear();
+    _sixthController.clear();
+    _seventhController.clear();
+  }
+
+  _openPopupCities(context) {
+    _clearControllers();
     Alert(
         context: context,
-        title: "LOGIN",
+        title: "Місто",
         content: Column(
           children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.account_circle),
-                labelText: 'Username',
+            Container(
+              child: InputField(
+                label: 'Назва',
+                content: 'Коломия',
+                controller: _firstController,
               ),
             ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                icon: Icon(Icons.lock),
-                labelText: 'Password',
+            Container(
+              child: InputField(
+                label: 'Країна',
+                content: 'Україна',
+                controller: _secondController,
               ),
             ),
           ],
@@ -32,7 +54,7 @@ class _AdminPageState extends State<AdminPage> {
           DialogButton(
             onPressed: () => Navigator.pop(context),
             child: Text(
-              "LOGIN",
+              "Добавити",
               style: TextStyle(color: Colors.white, fontSize: 20),
             ),
           )
@@ -45,76 +67,35 @@ class _AdminPageState extends State<AdminPage> {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.red,
-              child: Text(
-                'Ахахахха, ти підарас',
-                style: TextStyle(
-                  fontSize: _width * 0.05,
+          Container(
+            margin: EdgeInsets.only(top: _width * 0.04),
+            width: _width * 0.06,
+            child: Text(
+              'Адмін панель',
+              style: TextStyle(
+                fontSize: _width * 0.03,
+              ),
+            ),
+          ),
+          Container(
+            child: Wrap(
+              direction: Axis.vertical,
+              spacing: _width * 0.03,
+              children: [
+                Container(
+                  width: _width * 0.013,
+                  height: _width * 0.03,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'cities',
+                      style: TextStyle(
+                        fontSize: _width * 0.03,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: _width * 0.02,
-              child: RaisedButton(
-                color: Colors.orange,
-                onPressed: () => _openPopup(context),
-                child:
-                    Text('cities', style: TextStyle(fontSize: _width * 0.014)),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: _width * 0.02,
-              child: RaisedButton(
-                color: Colors.yellow,
-                onPressed: () {},
-                child:
-                    Text('cities_from', style: TextStyle(fontSize: _width * 0.014)),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: _width * 0.02,
-              child: RaisedButton(
-                color: Colors.green,
-                onPressed: () {},
-                child:
-                    Text('country', style: TextStyle(fontSize: _width * 0.014)),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: _width * 0.02,
-              child: RaisedButton(
-                color: Colors.blue,
-                onPressed: () {},
-                child:
-                    Text('users', style: TextStyle(fontSize: _width * 0.014)),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: _width * 0.02,
-              child: RaisedButton(
-                color: Colors.purple,
-                onPressed: () {},
-                child:
-                    Text('_check', style: TextStyle(fontSize: _width * 0.014)),
-              ),
+              ],
             ),
           ),
         ],
